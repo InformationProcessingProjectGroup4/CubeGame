600c4389cd317752efff3cdd90b89301022265eb
arnavxkohli <arnavkohli2003@gmail.com>
2023-03-20 20:40:49 +0000

    leaderboard complete

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -17,7 +17,39 @@ public class Leaderboard : MonoBehaviour
     void Start()
     {
 
-        Debug.Log(ScenesManager._level);
+        if(ScenesManager._level == 1)
+        {
+            name1.text = ScenesManager.leadernames0[0].ToString();
+            name2.text = ScenesManager.leadernames0[1].ToString(); 
+            name3.text = ScenesManager.leadernames0[2].ToString();
+            name4.text = ScenesManager.leadernames0[3].ToString();
+            score1.text = ScenesManager.leaderscores0[0].ToString();
+            score2.text = ScenesManager.leaderscores0[1].ToString();
+            score3.text = ScenesManager.leaderscores0[2].ToString();
+            score4.text = ScenesManager.leaderscores0[3].ToString();
+        }
+        else if(ScenesManager._level == 2)
+        {
+            name1.text = ScenesManager.leadernames1[0].ToString();
+            name2.text = ScenesManager.leadernames1[1].ToString();
+            name3.text = ScenesManager.leadernames1[2].ToString();
+            name4.text = ScenesManager.leadernames1[3].ToString();
+            score1.text = ScenesManager.leaderscores1[0].ToString();
+            score2.text = ScenesManager.leaderscores1[1].ToString();
+            score3.text = ScenesManager.leaderscores1[2].ToString();
+            score4.text = ScenesManager.leaderscores1[3].ToString();
+        }
+        else
+        {
+            name1.text = ScenesManager.leadernames2[0].ToString();
+            name2.text = ScenesManager.leadernames2[1].ToString();
+            name3.text = ScenesManager.leadernames2[2].ToString();
+            name4.text = ScenesManager.leadernames2[3].ToString();
+            score1.text = ScenesManager.leaderscores2[0].ToString();
+            score2.text = ScenesManager.leaderscores2[1].ToString();
+            score3.text = ScenesManager.leaderscores2[2].ToString();
+            score4.text = ScenesManager.leaderscores2[3].ToString();
+        }
     }
 
 }
